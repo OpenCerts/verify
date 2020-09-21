cef595d23b62a0ae6a4eff7e184217af7382accb
Sim Boon Long <simboonlong@gmail.com>
2020-09-21 17:36:34 +0800

    feat: upgrade oa verify (#11)

--- src/verify.ts
+++ src/verify.ts
@@ -73,6 +73,12 @@ const storeToFragment = (registry: Registry, store: string): VerificationFragmen
   };
 };
 
+// local function to check data + data.issuers fields
+// don't use utils.isWrappedV2Document, as it only checks OpenAttestationDocument version
+const isWrappedV2Document = (document: any): document is WrappedDocument<v2.OpenAttestationDocument> => {
+  return document.data && document.data.issuers;
+};
+
 export const registryVerifier: Verifier<
   WrappedDocument<v2.OpenAttestationDocument> | WrappedDocument<v3.OpenAttestationDocument>,
   VerificationManagerOptions,
@@ -83,8 +89,13 @@ export const registryVerifier: Verifier<
       const documentData = getData(document);
       return documentData.proof.method === v3.Method.DocumentStore;
     }
-    const documentData = getData(document);
-    return documentData.issuers.some(issuer => "documentStore" in issuer || "certificateStore" in issuer);
+
+    if (isWrappedV2Document(document)) {
+      const documentData = getData(document);
+      return documentData.issuers.some(issuer => "documentStore" in issuer || "certificateStore" in issuer);
+    }
+
+    return false;
   },
   skip: () => {
     return Promise.resolve({
